baeb4586b3b2436184fd8b486e31a276feae5484
angus <lang@zjxu.org>
2019-04-15 21:33:52 +0800

    es5

--- js/loader.js
+++ js/loader.js
@@ -1,86 +1,76 @@
-class App {
+function App() {
 
-  gallery_root;
-  tab_root;
-  tab_elements;
+  this.gallery_root = document.getElementById('gallery');
+  this.tab_root = document.getElementById('tab');
+  this.tab_elements = [];
 
-  tab_name;
-  tab_base = './doc/';
-  tabs;
+  this.tab_name = '';
+  this.tab_base = './doc/';
+  this.tabs = new Map();
 
-  init() {
-    this.gallery_root = document.getElementById('gallery');
-    this.tab_root = document.getElementById('tab');
-    this.tabs = new Map();
-    this.tab_elements = [];
-    this.load_config(doc_config);
-  }
+  var scope = this;
 
-  load_config(config) {
+  this.load_config = function(config) {
     const names = Object.getOwnPropertyNames(config);
     const ctime_sort = function(a, b) {
       return config[a].ctime - config[b].ctime;
     }
     names.sort(ctime_sort);
-    this.tab_name = names[0];
+    scope.tab_name = names[0];
     for(let i = 0; i < names.length; ++i) {
       const name = names[i];
-      this.tabs.set(name, config[name]);
+      scope.tabs.set(name, config[name]);
       const t = document.createElement('div');
       t.className = 'tab-label tab-disabled';
       t.innerHTML = name;
       t.name = name;
       t.onclick = () => {
-        this.load_tab(name);
+        scope.load_tab(name);
       }
-      this.tab_elements.push(t);
-      this.tab_root.appendChild(t);
+      scope.tab_elements.push(t);
+      scope.tab_root.appendChild(t);
     }
   }
 
-  launch() {
-    this.init();
+  this.launch = function() {
+    scope.load_config(doc_config);
+    scope.load_tab(scope.tab_name);
   }
 
-  deactivate_tab() {
-    this.tab_elements.forEach(tab => {
+  scope.deactivate_tab = function() {
+    scope.tab_elements.forEach(tab => {
       tab.className = 'tab-label tab-disabled';
     });
   }
 
-  active_tab(name) {
-    this.tab_elements.forEach(tab => {
+  scope.active_tab = function(name) {
+    scope.tab_elements.forEach(tab => {
       if (tab.name === name) {
         tab.className = 'tab-label tab-enabled';
       }
     });
   }
 
-  async load_tab(name) {
-    this.tab_name = name;
-    const tab_info = this.tabs.get(name);
-    const response = await fetch(this.tab_base + tab_info.file);
+  this.load_tab = async function(name) {
+    scope.tab_name = name;
+    const tab_info = scope.tabs.get(name);
+    const response = await fetch(scope.tab_base + tab_info.file);
     const content = marked(await response.text());
     const date = new Date(tab_info.ctime);
-    this.gallery_root.innerHTML = content + `<p class="date">${date.toDateString()}</p>`;
+    scope.gallery_root.innerHTML = content + `<p class="date">${date.toDateString()}</p>`;
 
-    this.deactivate_tab();
-    this.active_tab(name);
+    scope.deactivate_tab();
+    scope.active_tab(name);
 
     document.querySelectorAll('pre code').forEach((block) => {
       hljs.highlightBlock(block);
     });
   }
-
 }
 
 function main() {
-
   const app = new App();
-
   app.launch();
-  app.load_tab('Hello');
-
   window.app = app;
 }
 
