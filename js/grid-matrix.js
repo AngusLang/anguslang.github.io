5fe18708fd4a7972041f2430d877cd8292e5e0cd
lang <lilang8936@gmail.com>
2020-07-06 16:57:14 +0800

    update hue animation

--- js/grid-matrix.js
+++ js/grid-matrix.js
@@ -1,11 +1,61 @@
-const grid_width = 18;
-const grid_height = 10;
+const grid_width = 10;
+const grid_height = 15;
+
+/**
+ * Converts an HSL color value to RGB. Conversion formula
+ * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+ * Assumes h, s, and l are contained in the set [0, 1] and
+ * returns r, g, and b in the set [0, 255].
+ * 
+ * https://axonflux.com/handy-rgb-to-hsl-and-rgb-to-hsv-color-model-c
+ *
+ * @param   Number  h       The hue
+ * @param   Number  s       The saturation
+ * @param   Number  l       The lightness
+ * @return  Array           The RGB representation
+ */
+function hsl_to_rgb(h, s, l) {
+  var r, g, b;
+
+  if (s == 0) {
+    r = g = b = l; // achromatic
+  } else {
+    function hue2rgb(p, q, t) {
+      if (t < 0) t += 1;
+      if (t > 1) t -= 1;
+      if (t < 1 / 6) return p + (q - p) * 6 * t;
+      if (t < 1 / 2) return q;
+      if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+      return p;
+    }
+
+    var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+    var p = 2 * l - q;
+    r = hue2rgb(p, q, h + 1 / 3);
+    g = hue2rgb(p, q, h);
+    b = hue2rgb(p, q, h - 1 / 3);
+  }
+
+  return [r * 255, g * 255, b * 255];
+}
+
+function fract(i) {
+  return i - (i | 0);
+}
+
+function loop(i) {
+  if (i > 0.5) {
+    return (1.0 - i) * 2.0;
+  }
+  return i * 2.0;
+}
 
 function create_cell() {
   const cell = document.createElement('div');
   cell.className = 'grid-cell';
-  cell.style.width = 100 / grid_width + '%';
-  cell.style.height = 100 / grid_height + '%';
+  cell.style.width = '200px';
+  cell.style.height = '200px';
+  cell.style.borderRadius = '20px';
   return cell;
 }
 
@@ -36,11 +86,20 @@ const grid_animation = (time) => {
   for (let i = 0; i < grid_height; ++i) {
     for (let j = 0; j < grid_width; ++j) {
       const cell = cells[i * grid_width + j];
-      const height = sin_wave(i * 0.3, j * 0.3, time * 0.00025);
-      cell.style.transform = `translateZ(${height * 30}px)`;
 
-      const c = (height + 1.0) * 50 + 150;
-      cell.style.backgroundColor = `rgb(${c}, ${c}, ${c})`;
+      const x = cell.offsetLeft / window.innerWidth * 6.0;
+      const y = cell.offsetTop / window.innerHeight * 6.0;
+
+      const height = sin_wave(x * 0.3, y * 0.3, time * 0.00025);
+      cell.style.transform = `translateZ(${height * 40}px)`;
+
+      const c = (height + 1.0) * 100 + 150;
+
+      const hue = Math.sin(loop(fract(time * 0.0002)) * 0.05 * Math.PI);
+      const sat = 0.2;
+      const lit = 0.4 + (c / 255) * 0.4;
+      const rgb = hsl_to_rgb(hue, sat, lit);
+      cell.style.backgroundColor = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
     }
   }
   requestAnimationFrame(grid_animation);
